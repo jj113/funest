57f86bf7e039a7fa1797e807535c06f4a142337f
jj113 <58660794+jj113@users.noreply.github.com>
2020-11-29 10:32:08 -0600

    Update fit_fun.R

--- fit_fun.R
+++ fit_fun.R
@@ -1,6 +1,6 @@
-funest_fit = function(long_train, surv_train,
+funest_fit = function(long_train, surv_train, 
                       noftree = 500, nofcov = 2, split_rule = "maxstat",
-                      tv_names, fv_names,
+                      tv_names, fv_names, nbasis = 3, 
                       nofp = 3, t_star, t_pred, ...){
   # find number of unique individuals in training sample and test sample
   max_time = max(surv_train$time)
@@ -12,37 +12,42 @@ funest_fit = function(long_train, surv_train,
   }
   train_patID = surv_train$id
   train_npat = length(train_patID)
-  
-  #
   n_of_tv = length(tv_names)
-  train_tv_list = gen_tv_list(n_of_tv)
-  train_time_list = list()
-  for(i in 1:train_npat){
-    tid = train_patID[i]
-    tdat = long_train[long_train$id == tid,]
-    train_time_list = c(train_time_list, list(tdat$obstime))
-    temp_tv = tdat[, unlist(tv_names)]
-    for(j in 1:n_of_tv){
-      ttv = as.vector(temp_tv[j])[,1]
-      train_tv_list[[j]] = c(train_tv_list[[j]], list(ttv))
-    }
+  
+  # transfer longitudinal outcomes from long to wide
+  multivar.train = array(NA, c(nrow(surv_train), length(unique(long_train$obstime)), n_of_tv))
+  sums = 1
+  for(i in train_patID){
+
+    visits = long_train$visit[long_train$id == i]
+    multivar.train[sums,visits, 1] = long_train$Y1[long_train$id == i]
+    multivar.train[sums,visits, 2] = long_train$Y2[long_train$id == i]
+    multivar.train[sums,visits, 3]= long_train$Y3[long_train$id == i]
+    
+    sums = sums + 1
   }
   
-  train_tfobj = gen_tv_list(n_of_tv)
-  for(k in 1:n_of_tv){
-    train_tfobj[[k]] = funData::as.funData(funData::irregFunData(
-      argvals = train_time_list, X = train_tv_list[[k]]))
+  #---- user note: you may want to standardize your time-varying variables 
+  # univariate FPCA via PACE
+  Xi.train = L = phi.train = meanFun.train = varFun.train =  NULL
+  
+  for(p in 1:n_of_tv){
+    tmp.ufpca = uPACE(multivar.train[,,p], unique(long_train$obstime), nbasis=nbasis)
+    Xi.train = cbind(Xi.train, tmp.ufpca$scores) 
+    L = c(L, dim(tmp.ufpca$scores)[2])
+    phi.train[[p]] = t(tmp.ufpca$functions@X) 
+    meanFun.train[[p]] = tmp.ufpca$mu@X 
+    varFun.train[[p]] = tmp.ufpca$estVar@X 
   }
   
-  train_mfobj = funData::multiFunData(train_tfobj)
+  # multivariate FPCA
+  mFPCA.train = mFPCA(Xi=Xi.train, phi=phi.train, p=n_of_tv, L=L, nsurv = nrow(surv_train) )
+  rho.train = mFPCA.train$rho 
+  pve = mFPCA.train$pve
+  psi = mFPCA.train$psi
+  Cms = mFPCA.train$Cms
   
-  arg_list = gen_arg_list(n_of_tv, nofp)
-  
-  train_mfpca = MFPCA::MFPCA(train_mfobj, M = nofp,
-                             uniExpansions = arg_list)
-  
-  train_scores = train_mfpca$scores
-  train_funs = train_mfpca$vectors
+  train_scores = mFPCA.train$rho[,c(1:nbasis)] 
   
   train_covs = cbind(train_scores, surv_train[,unlist(fv_names)])
   d_delta = ncol(train_covs)
@@ -69,7 +74,12 @@ funest_fit = function(long_train, surv_train,
     fmla = fmla,
     score_names = score_names,
     nofp = nofp,
-    train_data.sub = train_data.sub
+    train_data.sub = train_data.sub,
+    phi.train = phi.train,
+    Xi.train = Xi.train,
+    nbasis = nbasis,
+    L = L,
+    multivar.train = multivar.train
   )
   
   return(list(misc = misc,
